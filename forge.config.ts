fd998594c455448ec806b800c43dd937fd8dbff1
Sandheep Rajkumar <rajkumar.sandheep@gmail.com>
2025-04-19 11:41:20 +0530

    Update dependencies to version 7.8.0, add fin.md to .gitignore, and remove appId from forge configuration for improved security.

--- forge.config.ts
+++ forge.config.ts
@@ -10,7 +10,6 @@ import { FuseV1Options, FuseVersion } from '@electron/fuses';
 const config: ForgeConfig = {
   packagerConfig: {
     asar: true,
-    appId: 'com.sonicflow.app',
   },
   rebuildConfig: {},
   makers: [new MakerSquirrel({}), new MakerZIP({}, ['darwin']), new MakerRpm({}), new MakerDeb({})],
